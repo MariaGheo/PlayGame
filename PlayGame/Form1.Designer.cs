78b42ea00f5b10be93a0b545c098f3b91563a6ba
marigheo340 <marigheo340@SNS-D-130S0R>
2022-02-09 14:04:56 -0500

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playGameButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playGameButton
+            // 
+            this.playGameButton.BackColor = System.Drawing.Color.Black;
+            this.playGameButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.playGameButton.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playGameButton.ForeColor = System.Drawing.Color.White;
+            this.playGameButton.Location = new System.Drawing.Point(317, 164);
+            this.playGameButton.Name = "playGameButton";
+            this.playGameButton.Size = new System.Drawing.Size(192, 65);
+            this.playGameButton.TabIndex = 1;
+            this.playGameButton.Text = "Play Game";
+            this.playGameButton.UseVisualStyleBackColor = false;
+            this.playGameButton.Click += new System.EventHandler(this.playGameButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.AutoSize = true;
+            this.countdownLabel.BackColor = System.Drawing.Color.Transparent;
+            this.countdownLabel.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.White;
+            this.countdownLabel.Location = new System.Drawing.Point(271, 206);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(0, 44);
+            this.countdownLabel.TabIndex = 3;
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(823, 445);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.playGameButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "The Most Amazing Game Ever (100% True)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button playGameButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; //allows the use of Thread.Sleep()
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playGameButton_Click(object sender, EventArgs e)
+        {
+            //make button "disappear"
+            playGameButton.Visible = false;
+
+            //change the countdownLabel text to begin countdown
+            countdownLabel.Text = "Game starts in... 3";
+
+            //redraw the screen to show updates then pause for 1 second 
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change the countdownLabel text to begin countdown
+            countdownLabel.Text = "Game starts in... 2";
+
+            //redraw the screen to show updates then pause for 1 second 
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change the countdownLabel text to begin countdown
+            countdownLabel.Text = "Game starts in... 1";
+
+            //redraw the screen to show updates then pause for 1 second 
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change the countdownLabel text to say "Go!" and change screen background to green
+            countdownLabel.Text = "Go!!!";
+            BackColor = Color.DarkGreen;
+        }
+    }
+}
